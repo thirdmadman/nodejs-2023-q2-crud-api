f712bb5529040b50816e68f796cab5831c304647
thirdmadman <thirdmadman@gmail.com>
2023-06-29 00:42:58 +0200

    feat: add interface DB

--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,11 @@
-export const db = {
+import { UserDTO } from './interfaces/dto/UserDTO';
+
+export interface DB {
+  users: Array<UserDTO>;
+  data: Array<string>;
+}
+
+export const db: DB = {
   users: [],
+  data: [],
 };
