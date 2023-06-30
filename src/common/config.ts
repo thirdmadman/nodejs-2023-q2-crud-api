a3563237d44d71f8918bb63bb33c1492ef2c6f04
thirdmadman <thirdmadman@gmail.com>
2023-07-01 00:18:45 +0200

    feat: correct dotenv.config

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,9 +1,6 @@
 import dotenv from 'dotenv';
-import path from 'path';
 
-dotenv.config({
-  path: path.join(__dirname, '../../.env'),
-});
+dotenv.config();
 
 export const PORT = process.env.PORT || '8000';
 export const HOST = process.env.HOST || 'localhost';
