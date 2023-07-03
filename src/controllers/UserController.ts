e7b65cf97df94ed976c9ae33a4d90921dc3d8416
thirdmadman <thirdmadman@gmail.com>
2023-07-03 13:13:48 +0200

    feat: add userDTO validation

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,10 @@
 import { validate } from 'uuid';
 /* eslint-disable class-methods-use-this */
 import { IncomingMessage, ServerResponse } from 'http';
+import { UserDTO } from '../interfaces/dto/UserDTO';
 import { IRequestParams } from '../interfaces/IRequestParams';
 import { UserRepository } from '../UserRepository';
 import { Controller } from './Controller';
-import { UserDTO } from '../interfaces/dto/UserDTO';
 
 export class UserController extends Controller {
   sendError(resp: ServerResponse, code: number, message: string) {
@@ -23,6 +23,34 @@ export class UserController extends Controller {
           resolve(body);
         });
     });
+  }
+
+  validateEntity(userDTO: UserDTO) {
+    if (!userDTO) {
+      return false;
+    }
+
+    if (!userDTO.name || typeof userDTO.name !== 'string' || userDTO.name === '') {
+      return false;
+    }
+
+    if (!userDTO.age || typeof userDTO.age !== 'number' || userDTO.age <= 0) {
+      return false;
+    }
+
+    if (!userDTO.hobbies || !Array.isArray(userDTO.hobbies)) {
+      return false;
+    }
+
+    if (userDTO.hobbies.length > 0) {
+      for (let i = 0; i < userDTO.hobbies.length; i++) {
+        if (typeof userDTO.hobbies[i] !== 'string' || userDTO.hobbies[i] === '') {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
   onGet(req: IRequestParams, resp: ServerResponse) {
@@ -56,6 +84,13 @@ export class UserController extends Controller {
     try {
       const data = await this.getBody(req.src);
       const jsonObj = JSON.parse(data) as UserDTO;
+
+      const isValid = this.validateEntity(jsonObj);
+      if (!isValid) {
+        this.sendError(resp, 400, 'Incorrect data: provided json is invalid schema of User');
+        return;
+      }
+
       if (jsonObj) {
         const user = UserRepository.create(jsonObj);
         const respObj = { data: user };
@@ -63,8 +98,7 @@ export class UserController extends Controller {
         resp.end(JSON.stringify(respObj));
       }
     } catch (err) {
-      this.sendError(resp, 400, 'Incorrect request');
-      console.error(err);
+      this.sendError(resp, 400, 'Incorrect data: provided json object is not valid.');
     }
   }
 
@@ -84,6 +118,13 @@ export class UserController extends Controller {
     try {
       const data = await this.getBody(req.src);
       const jsonObj = JSON.parse(data) as UserDTO;
+
+      const isValid = this.validateEntity(jsonObj);
+      if (!isValid) {
+        this.sendError(resp, 400, 'Incorrect data: provided json is invalid schema of User');
+        return;
+      }
+
       if (jsonObj) {
         const user = UserRepository.updateByUUID(req.pathParam, jsonObj);
         if (!user) {
@@ -95,8 +136,7 @@ export class UserController extends Controller {
         resp.end(JSON.stringify(respObj));
       }
     } catch (err) {
-      this.sendError(resp, 400, 'Incorrect request');
-      console.error(err);
+      this.sendError(resp, 400, 'Incorrect data: provided json object is not valid.');
     }
   }
 
